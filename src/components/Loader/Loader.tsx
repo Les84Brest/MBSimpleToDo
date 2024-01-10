9444e9c47eb8973cb5e756f0dd1fbe823677f3bb
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-01-10 17:55:39 +0300

    LazyLoad: add loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import { Box, CircularProgress } from "@mui/material";
+import { FC } from "react";
+
+export const Loader: FC = () => {
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+                <CircularProgress />
+            </Box>
+        </>
+    )
+}
+export default Loader
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { FC, lazy, Suspense } from 'react';
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import { Container } from '@mui/material';
+import Loader from '../components/Loader';
 import BottomNavigator from '../components/BottomNavigator/BottomNavigator';
 
 export const MainLayout: FC = () => {
@@ -9,9 +10,9 @@ export const MainLayout: FC = () => {
         <>
             <Header />
             <Container>
-                <Suspense fallback={<div>Loading... </div>}>
+                <Suspense fallback={<Loader />}>
                     <Outlet />
-                </Suspense>                
+                </Suspense>                         
             </Container>
             <BottomNavigator />
         </>
