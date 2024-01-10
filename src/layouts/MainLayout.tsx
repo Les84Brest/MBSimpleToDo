fd2561cdc8cbd057a4c978ec946a7dcb5c30bd4a
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-01-10 17:44:19 +0300

    LazyLoad: add main lazy load functionality

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,19 @@
-import {FC} from 'react';
-import {Outlet} from "react-router-dom";
+import { FC, lazy, Suspense } from 'react';
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import {Container} from "@mui/material";
+import { Container } from '@mui/material';
+import BottomNavigator from '../components/BottomNavigator/BottomNavigator';
 
 export const MainLayout: FC = () => {
     return (
         <>
-            <Header/>
+            <Header />
             <Container>
-                <Outlet/>
+                <Suspense fallback={<div>Loading... </div>}>
+                    <Outlet />
+                </Suspense>                
             </Container>
+            <BottomNavigator />
         </>
     );
 }
